9ff375d74e6e9972bffae591a0a3d0a72e0e057b
dam <mbelosoleon@danielcastelao.org>
2025-11-18 12:57:39 +0100

    IU

--- app/src/main/java/com/dam/lectorarchivos/Datos.kt
+++ app/src/main/java/com/dam/lectorarchivos/Datos.kt
@@ -1,7 +1,13 @@
 package com.dam.lectorarchivos
 
-enum class Estados (){
-    INICIO(),
-    CARGANDO(),
-    FINALIZANDO
+import androidx.compose.ui.graphics.Color
+
+enum class Estados(val start_activo: Boolean, val boton_activo: Boolean) {
+    INICIO(start_activo = true, boton_activo = false),
+    CARGANDO(start_activo = false, boton_activo = false),
+    FINALIZANDO(boton_activo = false, start_activo = false)
+}
+
+enum class Colores(val color: Color, val txt: String) {
+    CLASE_ROJO(color = Color.Red, txt = "Rojo"),
 }
--- app/src/main/java/com/dam/lectorarchivos/IU.kt
+++ app/src/main/java/com/dam/lectorarchivos/IU.kt
@@ -1,18 +1,25 @@
 package com.dam.lectorarchivos
 
 import android.text.Layout
+import android.util.Log
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun IU(miViewModel: MyViewModel) {
@@ -23,10 +30,13 @@ fun IU(miViewModel: MyViewModel) {
 fun Interfaz(miViewModel: MyViewModel) {
 
     val progresoObtenido by miViewModel._progresoFlow.collectAsState()
+    val estadoObtenido by miViewModel._estadoFlow.collectAsState()
 
     Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center){
         Column(modifier = Modifier.padding(16.dp), horizontalAlignment = Alignment.CenterHorizontally) {
             Progreso(progresoObtenido)
+            Estado(estadoObtenido)
+            Boton(miViewModel, Colores.CLASE_ROJO)
         }
     }
 
@@ -35,9 +45,33 @@ fun Interfaz(miViewModel: MyViewModel) {
 
 @Composable
 fun Progreso(progreso: Int){
-    Text(text = "progreso: $progreso")
+    Text(text = "progreso: $progreso%")
 }
 
+@Composable
+fun Estado(estado: Estados){
+    Text(text = "estado: $estado")
+}
+
+@Composable
+fun Boton(viewModel: MyViewModel, enum_color: Colores) {
+    val activo = viewModel._estadoFlow.collectAsState().value
+    Button(
+        enabled = activo.boton_activo,
+        colors = ButtonDefaults.buttonColors(enum_color.color),
+        onClick = {
+            Log.d("Juego", enum_color.txt + " numero: " + enum_color.ordinal)
+        },
+        shape = RoundedCornerShape(0.dp),
+        modifier = Modifier.size(150.dp).padding(15.dp)
+    ) {
+        Text(
+            text = enum_color.txt,
+            fontSize = 15.sp,
+            color = Color.Black
+        )
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
